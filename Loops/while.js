2b004473763048f5046e145dced7c910fa466f33
lokesh-patil57 <luckypatil577R@gmail.com>
2025-02-17 23:14:28 +0530

    commit

--- Loops/while.js
+++ Loops/while.js
@@ -2,5 +2,10 @@ let favanime = "Naruto"
 let guess = prompt("Enter My Favourite Anime Name")
 while (guess != favanime || guess == "quit"  ) {
     console.log('Wrong');
-    guess = prompt("")
+    guess = prompt("Wrong Guess Please try again")
+}
+
+if (guess == favanime) {
+    alert('Congrats!!!');
+    
 }
