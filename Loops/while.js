0a372fa1f869cf55a65653d4b7217d53bce1afea
lokesh-patil57 <luckypatil577R@gmail.com>
2025-02-17 23:16:33 +0530

    added quit

--- Loops/while.js
+++ Loops/while.js
@@ -1,6 +1,10 @@
 let favanime = "Naruto" 
 let guess = prompt("Enter My Favourite Anime Name")
-while (guess != favanime || guess == "quit"  ) {
+while (guess != favanime ) {
+    if (guess == "quit"  ) {
+        console.log('You Quit');
+        break
+    }
     console.log('Wrong');
     guess = prompt("Wrong Guess Please try again")
 }
