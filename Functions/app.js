c1a5131f21a8cd536d26d72418a2069fdc4f6e95
lokesh-patil57 <luckypatil577R@gmail.com>
2025-02-20 23:02:29 +0530

    Average of two no

---
+++ Functions/app.js
@@ -0,0 +1,10 @@
+function die () {
+    dieNum = Math.floor(Math.random()*6)+1
+    console.log('die rolled:',dieNum);
+    
+} 
+function avg (a,b,c){
+    Average = a+b+c/3
+    console.log('Your average is :',Average);
+    
+}
