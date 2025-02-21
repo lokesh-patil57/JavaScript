721f840908b7cc297ec6aa9fb837c0357a303d1e
lokesh-patil57 <luckypatil577R@gmail.com>
2025-02-21 19:48:05 +0530

    concatenation of string

--- Functions/app.js
+++ Functions/app.js
@@ -1,33 +1,39 @@
-function die () {
-    dieNum = Math.floor(Math.random()*6)+1
-    console.log('die rolled:',dieNum);
-    
-} 
-function avg (a,b,c){
-    Average = (a+b+c)/3
-    console.log('Your average is :',Average);
-    
+function die() {
+  dieNum = Math.floor(Math.random() * 6) + 1;
+  console.log("die rolled:", dieNum);
+}
+function avg(a, b, c) {
+  Average = (a + b + c) / 3;
+  console.log("Your average is :", Average);
 }
 
 function table(num) {
-    for(let i = num ; i <= num*10 ; i+=num ) {
+  for (let i = num; i <= num * 10; i += num) {
     console.log(i);
-    }
+  }
 }
 
-function sum(a,b) {
-    return a+b
+function sum(a, b) {
+  return a + b;
 }
- console.log( sum(1,2));
- console.log(sum(sun(1,2),3));
+console.log(sum(1, 2));
+console.log(sum(sum(1, 2), 3));
 
+function noSum(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
 
- function noSum (n) {
-    let sum = 0
-    for(let i = 1; i <= n; i++) {
-        sum = sum+i
-        
-    }
-    return sum
- }
- 
+// concatenation of string
+let str = ["hi", "hello", "bye", "see you"];
+
+function concat(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += str[i];
+  }
+  return result;
+}
