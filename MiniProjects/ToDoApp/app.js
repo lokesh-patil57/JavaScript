20a563990a73fe8bdce7e7f46ea05fb87aa109b1
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-07 00:02:34 +0530

    To do complete

--- MiniProjects/ToDoApp/app.js
+++ MiniProjects/ToDoApp/app.js
@@ -2,8 +2,32 @@ let inp = document.querySelector("input");
 let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
 btn.addEventListener("click", function () {
-    let item = document.createElement("li");
+  let item = document.createElement("li");
   item.textContent = inp.value;
+
+  let delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.classList.add("delete");
+
+  item.appendChild(delBtn);
   ul.appendChild(item);
   inp.value = "";
 });
+
+
+ul.addEventListener( 'click' , function (event) {
+  if (event.target.nodeName == "BUTTON") {
+    let listItem = event.target.parentElement
+    listItem.remove()
+    console.log('Deleted');
+  }
+});
+
+// let delBtns = document.querySelectorAll(".delete");
+// for (delBtn of delBtns) {
+//   delBtn.addEventListener("click", function () {
+//     let par = this.parentElement;
+//     console.log(par);
+//     par.remove();
+//   });
+// }
