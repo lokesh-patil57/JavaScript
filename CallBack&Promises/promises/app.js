9d8863ad7f9d697d01c0f8f3af4b4e0064bde115
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-12 23:39:24 +0530

    setting Promies

---
+++ CallBack&Promises/promises/app.js
@@ -0,0 +1,11 @@
+function savetoDb (data) {
+return Promise((resolve , reject) => {
+    let internetSpeed = Math.floor(Math.random()*10)+1
+    if (internetSpeed>4) {
+        resolve("Success :Data Saved to DataBase")
+    } else{
+        reject("Failuare: Weak connection")
+    }  
+}
+)
+}
