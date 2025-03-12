811ada5eee118726e8a75075e3de4c4e5d9bc75b
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-13 00:26:22 +0530

    call back hell resolved with Promises

---
+++ CallBack&Promises/CallBack Hell/script.js
@@ -0,0 +1,37 @@
+let h1 = document.querySelector("h1");
+
+function changeColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      h1.style.color = color;
+      resolve("Color Changed");
+    }, delay);
+  });
+}
+
+changeColor("red", 1000)
+  .then(() => {
+    console.log("Color Changed to Red");
+    return changeColor("green", 1000);
+  })
+  .then(() => {
+    console.log("Color Changed to Green");
+    return changeColor("black", 5000);
+  })
+  .then(() => {
+    console.log("Color Changed to Black");
+  })
+
+  .catch((error) => {
+    console.log("Color Not Changed");
+  });
+
+// changeColor("red", 1000, () => {
+//   changeColor("green", 1000, () => {
+//     changeColor("blue", 1000, () => {
+//       changeColor("yello", 1000, () => {
+//         changeColor("purple", 1000);
+//       });
+//     });
+//   });
+// });
