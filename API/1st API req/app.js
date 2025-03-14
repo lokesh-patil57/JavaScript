39319203d770d0a596be2d61a207208a4eef25dc
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-15 00:17:36 +0530

    API call With Async Function

--- API/1st API req/app.js
+++ API/1st API req/app.js
@@ -1,16 +1,35 @@
-let url = "https://catfact.ninja/fact";
-fetch(url)
-  .then((res) => {
-    console.log(res);
-    return res.json()
-  })
-  .then((data) =>{
-    console.log("Data Fact =",data.fact);
+// let url = "https://catfact.ninja/fact";
+// fetch(url)
+//   .then((res) => {
+//     console.log(res);
+//     return res.json()
+//   })
+//   .then((data) =>{
+//     console.log("Data Fact =",data.fact);
     
-  })
-  .catch((err) => {
-    console.log(`Error 404`, err);
-  });
+//   })
+//   .catch((err) => {
+//     console.log(`Error 404`, err);
+//   });
 
-fetch(url);
+// fetch(url);
 
+
+let url = "https://catfact.ninja/fact";
+
+async function getReq() {
+  try {
+    let res = await fetch(url)
+  let data  = await res.json()
+  console.log(data);
+  console.log("fact =",data.fact);
+  
+  
+  } catch (error) {
+    console.log(`Error 404`);
+    
+  }
+  
+}
+
+getReq()
