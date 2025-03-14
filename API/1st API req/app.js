e99e7d5e80cb7a62f67b60d4e728d1e812b70ca4
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-15 00:11:07 +0530

    then catch to API

--- API/1st API req/app.js
+++ API/1st API req/app.js
@@ -2,10 +2,11 @@ let url = "https://catfact.ninja/fact";
 fetch(url)
   .then((res) => {
     console.log(res);
-    res.json().then((data) =>{
-      console.log(data);
-      
-    })
+    return res.json()
+  })
+  .then((data) =>{
+    console.log("Data Fact =",data.fact);
+    
   })
   .catch((err) => {
     console.log(`Error 404`, err);
