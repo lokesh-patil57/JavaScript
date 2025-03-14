ea0cd19f15bf7254ab35114304a600639b694ff4
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-15 00:01:42 +0530

    data added

--- API/1st API req/app.js
+++ API/1st API req/app.js
@@ -2,6 +2,10 @@ let url = "https://catfact.ninja/fact";
 fetch(url)
   .then((res) => {
     console.log(res);
+    res.json().then((data) =>{
+      console.log(data);
+      
+    })
   })
   .catch((err) => {
     console.log(`Error 404`, err);
