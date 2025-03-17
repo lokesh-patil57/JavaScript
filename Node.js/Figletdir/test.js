904623b9eb1169ec7234de0c1f02ea84a67eaa16
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-17 23:58:15 +0530

    usage of dependecies

--- Node.js/Figletdir/test.js
+++ Node.js/Figletdir/test.js
@@ -1,6 +1,14 @@
 const figlet = require("figlet");
 
-figlet("Hello, World!", function (err, data) {
+figlet("L u c k y P a t i l", function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+figlet("D e v e l o p e r", function (err, data) {
     if (err) {
         console.log("Something went wrong...");
         console.dir(err);
