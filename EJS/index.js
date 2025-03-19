f392283a2214619b1146e3f5836a53e5e6ca7a4f
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-19 19:24:09 +0530

    Instagram EJS

--- EJS/index.js
+++ EJS/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-
 let port = 8080;
 
 app.listen(port, () => {
@@ -12,6 +11,13 @@ app.listen(port, () => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/", (req, res) => {
-  res.render("home.ejs");
+app.get("/rolldie", (req, res) => {
+  let dieRoll = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { num: dieRoll });
 });
+
+app.get("/ig/:username", (req, res) => {
+    let {username} = req.params
+    res.render("instagram.ejs", { username });
+  });
+  
