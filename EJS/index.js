f9f5372d30657b27be14996f873de14448f98b74
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-19 18:58:23 +0530

    views Directory

--- EJS/index.js
+++ EJS/index.js
@@ -1,15 +1,17 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-let port = 8080
 
-app.listen( port , () => {
-    console.log(`app is listening on port 8080`);
-    
-})
+let port = 8080;
 
-app.set("view engine" , "ejs")
+app.listen(port, () => {
+  console.log(`app is listening on port 8080`);
+});
 
-app.get("/", (req , res )=>{
-    res.render("home.ejs")
-})
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
